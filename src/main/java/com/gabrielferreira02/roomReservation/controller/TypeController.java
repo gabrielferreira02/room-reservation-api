9d15ee59f8bc636a252b54dd8b29c1e5191fc1ed
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-01-16 13:58:38 -0300

    Changed methods name

--- src/main/java/com/gabrielferreira02/roomReservation/controller/TypeController.java
+++ src/main/java/com/gabrielferreira02/roomReservation/controller/TypeController.java
@@ -5,6 +5,7 @@ import com.gabrielferreira02.roomReservation.entity.TypeEntity;
 import com.gabrielferreira02.roomReservation.service.TypeServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,23 +20,27 @@ public class TypeController {
         this.typeService = typeService;
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping
     public List<TypeEntity> findAll() {
         return typeService.findAll();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<TypeEntity> createUser(@RequestBody @Valid TypeRequestDTO typeDTO) {
+    public ResponseEntity<TypeEntity> createType(@RequestBody @Valid TypeRequestDTO typeDTO) {
         return typeService.createType(typeDTO);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
-    public ResponseEntity<TypeEntity> updateUser(@RequestBody @Valid TypeRequestDTO typeDTO, @PathVariable Long id) {
+    public ResponseEntity<TypeEntity> updateType(@RequestBody @Valid TypeRequestDTO typeDTO, @PathVariable Long id) {
         return typeService.updateType(typeDTO, id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+    public ResponseEntity<?> deleteType(@PathVariable Long id) {
         return typeService.deleteType(id);
     }
 }
